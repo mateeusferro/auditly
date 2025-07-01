a744beebb1767ea000bc743c736d1300ad7478f8
mateus <mateus.03fernandesaraujo@gmail.com>
2025-06-30 21:36:02 -0300

    chore: change test dependency to jest

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import vitest from "@vitest/eslint-plugin";
 
 export default tseslint.config(
   {
@@ -28,11 +27,8 @@ export default tseslint.config(
   },
   {
     files: ["test/*.test.ts", "test/*.spec.ts"],
-    plugins: {
-      vitest,
-    },
+    plugins: {},
     rules: {
-      ...vitest.configs.recommended.rules,
       "@typescript-eslint/unbound-method": "off",
     },
   },
