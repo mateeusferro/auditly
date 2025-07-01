1506691c1c37d9efef9e6b215ad42cc74b27612b
mateus <mateus.03fernandesaraujo@gmail.com>
2025-06-30 21:24:34 -0300

    feat: create route to get log by id and by filter

--- src/interfaces/controllers/LogController.ts
+++ src/interfaces/controllers/LogController.ts
@@ -1,12 +1,16 @@
 import { CreateLogDto, CreateLogSchema } from "@/application/dtos/CreateLogDto";
+import { FilterQueryDto, FilterQuerySchema } from "@/application/dtos/FilterQueryDto";
 import { CreateLog } from "@/application/use-cases/CreateLog";
+import { GetLogById } from "@/application/use-cases/GetLogById";
+import { GetLogsByFilter } from "@/application/use-cases/GetLogsByFilter";
+import { Log } from "@/domain/entities/Log";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class LogController {
 
-    static async create(req: FastifyRequest, reply: FastifyReply): Promise<string> {
-        const logDto = req.body as CreateLogDto;
+    static async create(request: FastifyRequest, reply: FastifyReply): Promise<string> {
+        const logDto = request.body as CreateLogDto;
         const parsedBody = CreateLogSchema.safeParse(logDto);
 
         if (!parsedBody.success) {
@@ -16,5 +20,27 @@ export class LogController {
         const logCreated = await new CreateLog().execute(parsedBody.data);
 
         return reply.status(201).send({ id: logCreated });
+    }
+
+    static async getById(request: FastifyRequest, reply: FastifyReply): Promise<Log> {
+        const params = request.params as { id: string };
+
+        const log = await new GetLogById().execute(params.id);
+
+        return reply.status(200).send(log);
+    }
+
+    static async getByFilters(request: FastifyRequest, reply: FastifyReply): Promise<string> {
+        const queryParams = request.query as FilterQueryDto;
+        const parsedQuery = FilterQuerySchema.safeParse(queryParams);
+
+
+        if(!parsedQuery.success) {
+            return reply.status(400).send({ error: parsedQuery.error });
+        }
+
+        const result = await new GetLogsByFilter().execute(parsedQuery.data);
+
+        return reply.status(200).send(result);
     }
 }
--- src/interfaces/routes/LogRoute.ts
+++ src/interfaces/routes/LogRoute.ts
@@ -3,6 +3,7 @@ import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import { LogSchema } from "../validators/LogValidate";
 import { LogController } from "../controllers/LogController";
 import { IRouter } from "@/interfaces/routes/IRouter";
+import { FilterQueryDto } from "@/application/dtos/FilterQueryDto";
 
 export class LogRoute implements IRouter {
     public prefix = "/log";
@@ -17,6 +18,16 @@ export class LogRoute implements IRouter {
             },
             (request, reply) => 
                 LogController.create(request, reply)
+        );
+
+        fastify.get("/:id", 
+            (request, reply) =>
+                LogController.getById(request, reply)  
+        );
+
+        fastify.get<{ Querystring: FilterQueryDto }>("",
+            (request, reply) =>
+                LogController.getByFilters(request, reply)
         );
 
         _done();
