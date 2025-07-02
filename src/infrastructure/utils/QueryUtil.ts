1aceae5a84473233668a9694221b5b5868f349be
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-01 21:09:43 -0300

    refactor: create a function to build the sql filter

---
+++ src/infrastructure/utils/QueryUtil.ts
@@ -0,0 +1,34 @@
+import { Filter } from "@/domain/entities/Filter";
+
+export function buildFilter(filter: Filter): [unknown[], string[]] {
+    const { actor, action, resource, from, to } = filter;
+    const values: unknown[] = [];
+    const conditions: string[] = [];
+
+    if (actor) {
+        values.push(actor);
+        conditions.push(`ACTOR = $${String(values.length)}`);
+    }
+
+    if (action) {
+        values.push(action);
+        conditions.push(`ACTION = $${String(values.length)}`);
+    }
+
+    if (resource) {
+        values.push(resource);
+        conditions.push(`RESOURCE = $${String(values.length)}`);
+    }
+
+    if (from) {
+        values.push(from);
+        conditions.push(`TIMESTAMP >= $${String(values.length)}`);
+    }
+
+    if (to) {
+        values.push(to);
+        conditions.push(`TIMESTAMP <= $${String(values.length)}`);
+    }
+
+    return [values, conditions];
+}
