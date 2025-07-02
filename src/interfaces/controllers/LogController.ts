4c82619c4dfa6c4dd2c153f56b6c546c594e32d7
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-01 21:08:52 -0300

    feat: create endpoints to get logs with pagination, search and a summary of them

--- src/interfaces/controllers/LogController.ts
+++ src/interfaces/controllers/LogController.ts
@@ -1,9 +1,14 @@
 import { CreateLogDto, CreateLogSchema } from "@/application/dtos/CreateLogDto";
 import { FilterQueryDto, FilterQuerySchema } from "@/application/dtos/FilterQueryDto";
+import { Pageable, PageableDto, PageableSchemaDto } from "@/application/dtos/PageableDto";
 import { CreateLog } from "@/application/use-cases/CreateLog";
+import { GetAllPaginatedLogs } from "@/application/use-cases/GetAllPaginatedLogs";
 import { GetLogById } from "@/application/use-cases/GetLogById";
 import { GetLogsByFilter } from "@/application/use-cases/GetLogsByFilter";
+import { GetSummary } from "@/application/use-cases/GetSummary";
+import { SearchLogs } from "@/application/use-cases/SearchLogs";
 import { Log } from "@/domain/entities/Log";
+import { Summary } from "@/domain/entities/Summary";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
@@ -41,6 +46,34 @@ export class LogController {
 
         const result = await new GetLogsByFilter().execute(parsedQuery.data);
 
+        return reply.status(200).send(result);
+    }
+
+    static async getAll(request: FastifyRequest, reply: FastifyReply): Promise<Pageable<Log>> {
+        const queryParams = request.query as PageableDto;
+        console.log(queryParams);
+        const parsedQuery = PageableSchemaDto.safeParse(queryParams);
+
+        if(!parsedQuery.success) {
+            return reply.status(400).send({ error: parsedQuery.error });
+        }
+
+        const result = await new GetAllPaginatedLogs().execute(parsedQuery.data);
+
+        return reply.status(200).send(result);
+    }
+
+    static async search(request: FastifyRequest, reply: FastifyReply): Promise<Log[]> {
+        const { q } = request.query as { q: string };
+
+        const result = await new SearchLogs().execute(q);
+
+        return reply.status(200).send(result);
+    }
+
+    static async summary(_request: FastifyRequest, reply: FastifyReply): Promise<Summary> {
+        const result = await new GetSummary().execute();
+        
         return reply.status(200).send(result);
     }
 }
--- src/interfaces/routes/LogRoute.ts
+++ src/interfaces/routes/LogRoute.ts
@@ -4,6 +4,7 @@ import { LogSchema } from "../validators/LogValidate";
 import { LogController } from "../controllers/LogController";
 import { IRouter } from "@/interfaces/routes/IRouter";
 import { FilterQueryDto } from "@/application/dtos/FilterQueryDto";
+import { PageableDto } from "@/application/dtos/PageableDto";
 
 export class LogRoute implements IRouter {
     public prefix = "/log";
@@ -25,9 +26,24 @@ export class LogRoute implements IRouter {
                 LogController.getById(request, reply)  
         );
 
-        fastify.get<{ Querystring: FilterQueryDto }>("",
+        fastify.get<{ Querystring: FilterQueryDto }>("/filter",
             (request, reply) =>
                 LogController.getByFilters(request, reply)
+        );
+
+        fastify.get<{ Querystring: PageableDto }>("",
+            (request, reply) =>
+                LogController.getAll(request, reply)
+        );
+
+        fastify.get<{ Querystring: { q: string } }>("/search",
+            (request, reply) =>
+                LogController.search(request, reply)
+        );
+
+        fastify.get("/summary",
+            (request, reply) =>
+                LogController.summary(request, reply)
         );
 
         _done();
